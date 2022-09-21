6ad97a5e8168bed21d246ea08f62cb07ea1314f2
Vladislav Prozorov <prozorov93@gmail.com>
2022-09-21 22:00:23 +0300

    Code refactoring

--- cart/task.js
+++ cart/task.js
@@ -4,32 +4,41 @@ const cartList = document.querySelector('.cart__products')
 function addToCart(event) {
     const product = event.target.closest('.product')
     const cartProducts = Array.from(cartList.querySelectorAll('.cart__product'))
-    const productInCartProducts = cartProducts.filter((element) => {
+    // const productInCartProducts = cartProducts.filter((element) => {
+    //     return element.getAttribute('data-id') === product.getAttribute('data-id')
+    // })[0]
+    const productInCartProducts = cartProducts.find((element) => {
         return element.getAttribute('data-id') === product.getAttribute('data-id')
-    })[0]
+    })
+
 
     if (productInCartProducts) {
         let countCartProduct = productInCartProducts.querySelector('.cart__product-count')
         countCartProduct.textContent = Number(countCartProduct.textContent) + Number(product.querySelector('.product__quantity-value').textContent)
         return
-    } else {
-        const divProduct = document.createElement('div')
-        divProduct.classList.add('cart__product')
-        divProduct.setAttribute('data-id', product.getAttribute('data-id'))
-        
-        const imgProductImage = document.createElement('img')
-        imgProductImage.classList.add('cart__product-image')
-        imgProductImage.setAttribute('src', product.querySelector('.product__image').getAttribute('src'))
-        
-        const divProductCount = document.createElement('div')
-        divProductCount.classList.add('cart__product-count')
-        divProductCount.textContent = product.querySelector('.product__quantity-value').textContent
-        
-        divProduct.appendChild(imgProductImage)
-        divProduct.appendChild(divProductCount)
-
-        cartList.appendChild(divProduct)
     }
+    
+    const divProduct = document.createElement('div')
+    divProduct.classList.add('cart__product')
+    divProduct.setAttribute('data-id', product.getAttribute('data-id'))
+
+    divProduct.innerHTML = `
+    <img class="cart__product-image" src="${product.querySelector('.product__image').getAttribute('src')}">
+    <div class="cart__product-count">${product.querySelector('.product__quantity-value').textContent}</div>`
+    
+    // const imgProductImage = document.createElement('img')
+    // imgProductImage.classList.add('cart__product-image')
+    // imgProductImage.setAttribute('src', product.querySelector('.product__image').getAttribute('src'))
+    
+    // const divProductCount = document.createElement('div')
+    // divProductCount.classList.add('cart__product-count')
+    // divProductCount.textContent = product.querySelector('.product__quantity-value').textContent
+    
+    // divProduct.appendChild(imgProductImage)
+    // divProduct.appendChild(divProductCount)
+
+    cartList.appendChild(divProduct)
+    
 }
 
 function changeCount(event) {
--- todo/task.js
+++ todo/task.js
@@ -2,39 +2,63 @@ const textArea = document.querySelector('.tasks__input')
 const createTaskButton = document.querySelector('.tasks__add')
 let toDoList = document.querySelector('.tasks__list')
 const toDoStorage = window.localStorage;
+let localItemObjects = []
+
+function createTaskElement(value) {
+    const divTask = document.createElement('div')
+    divTask.classList.add('task')
+    divTask.innerHTML = `<div class="task__title">${value}</div><a href="#" class="task__remove">×</a>`
+    toDoList.appendChild(divTask)
+}
 
 function addTask(event) {
     if (event.type === 'click' && textArea.value.trim() != '') {
-        const divTask = document.createElement('div')
-        divTask.classList.add('task')
+        // const divTask = document.createElement('div')
+        // divTask.classList.add('task')
 
-        const divTaskText = document.createElement('div')
-        divTaskText.classList.add('task__title')
-        divTaskText.textContent = textArea.value
-        divTask.appendChild(divTaskText)
+        // const divTaskText = document.createElement('div')
+        // divTaskText.classList.add('task__title')
+        // divTaskText.textContent = textArea.value
+        // divTask.appendChild(divTaskText)
 
-        const divTaskRemove = document.createElement('a')
-        divTaskRemove.setAttribute('href', "#")
-        divTaskRemove.classList.add('task__remove')
-        divTaskRemove.textContent = '×'
-        divTask.appendChild(divTaskRemove)
+        // const divTaskRemove = document.createElement('a')
+        // divTaskRemove.setAttribute('href', "#")
+        // divTaskRemove.classList.add('task__remove')
+        // divTaskRemove.textContent = '×'
+        // divTask.appendChild(divTaskRemove)
 
-        toDoList.appendChild(divTask)
-        toDoStorage.setItem('toDoList', toDoList.innerHTML)
+        // const divTask = document.createElement('div')
+        // divTask.classList.add('task')
+        // divTask.innerHTML = `<div class="task__title">${textArea.value}</div><a href="#" class="task__remove">×</a>`
+        // toDoList.appendChild(divTask)
+
+        createTaskElement(textArea.value)
+        localItemObjects.push({value: textArea.value})
+        toDoStorage.setItem('toDoList', JSON.stringify({items: localItemObjects}))
+        textArea.value = ''
     }
     event.preventDefault()
 }
 
 function getTasks(event) {
     if (toDoStorage.getItem('toDoList')!=null) {
-        toDoList.innerHTML = toDoStorage.getItem('toDoList')
+        localItemObjects = JSON.parse(toDoStorage.getItem('toDoList')).items
+        localItemObjects.forEach((element) => {
+            createTaskElement(element.value)
+        })
     }
 }
 
 function removeTask(event) {
     const parent = event.target.closest('.task')
+    const valueOfRemovedElement = parent.querySelector('.task__title').textContent
+    for (let [index, value] of localItemObjects.entries()){
+        if (value.value === valueOfRemovedElement){
+            localItemObjects.splice(index, 1)
+        }
+    }
     parent.remove()
-    toDoStorage.setItem('toDoList', toDoList.innerHTML)
+    toDoStorage.setItem('toDoList', JSON.stringify({items: localItemObjects}))
     event.preventDefault()
 }
 
